eef57051018c25ef0412c3ad739bed44dcbb85c7
sunday00 <32473986+sunday00@users.noreply.github.com>
2020-07-05 23:09:24 +0900

    Update Index.svelte

---
+++ resources/components/About/Index.svelte
@@ -0,0 +1,126 @@
+<div class="wrap">
+    <h1 class="title">Me?</h1>
+
+    <ul class="top">
+        <li>I'm working as php web developer primary,</li>
+        <li>also possible to use spring, </li>
+        <li>and some more things...</li>
+    </ul>
+
+    <hr />
+
+    <div class="mt-10">
+        <article>
+            <h2><i class="fab fa-php text-purple-700"></i></h2>
+            <p>Mainly I'm using PHP. In Workplace, although I don't love to do, but I'm able to make web very old style, AKA legacy php.</p>
+            <i class="fab fa-laravel text-red-500 mt-5"></i>
+            <p>I experienced make web page via Laravel, so I hope you visit <a href="http://grayfield.net" target="_blank">grayfield.net</a>, that I write some skill learn recently or How I overcome struggle.</p>
+            <svg class="right-0 mt-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 75 75"><g transform="matrix(.485216 0 0 .485216 3.93025 0)"><path d="M57.6 76.6l-8.2 1.2-6.1 2.9 12.6-1 2.4-1.9z" fill="#acd2b4"/><path d="M73.7 128l-4.8-5.6-.4-5.7 2.2-4.9 6 2.8v1.8l-4.1-1.7-.5.9 3.8 1.9 1.6 3.9-3.8-3.2h-.8z" fill="#3a3a3a"/><path d="M69.6 117.7v4.6l-6.2 2.6-3.8-6.8V98.2l1.6-3.1.6.5.9 15.4z" fill="#8fc19a"/><path d="M81.3 111l-5.2-6-.2-4.5 2.3-5.6 6 2.8v1.9l-4-1.8-.5.9 3.6 1.8 1.7 4.1-3.6-3.3h-.8z" fill="#3a3a3a"/><path d="M76.7 101.1v3.7l-10 3.8-3.5-7.4-.8-4.8 2.3-1.5 6.8-.4 1.6 4.2z" fill="#678a6f"/><path d="M53.5 81.8l-18.8 18.7v12.2l4.7 16.1v-8.5L50.6 93l12.8-8.6zm41.5.3c-.2 0-12.1 3-12.1 3l-11.8 9.8h-6.4l19.5-12.8z" fill="#3a3a3a"/><path d="M88.8 75.8l-.5-5.9 11.8.8 10.8 3.1 4.8 7.7-5.7 3z" fill="#79a281"/><path d="M110.8 79.3l3 1.7-.2.4-2.8-1.6-1.9 1.4-.2-.3z" fill="#3a3a3a"/><path d="M61.2 68.1l3-6.8L86.9 67v13.1l-8.7-3.3z" fill="#8fc19a"/><path d="M92.5 63.6L82.3 53.5l.3-9.4z" fill="#7aa383"/><path d="M69.7 46.6l19.1 20.8-6.1-26.2L34.6 0l7.1 19.6z" fill="#cce2d2"/><path d="M51.4 40.1l18.3 6.5-37.4-36.2 1.6 9.7z" fill="#acd2b4"/><path d="M23.7 54.7L9.6 41.8l19.5 5.3 15.8 15.6-16.8-5z" fill="#393839"/><path d="M60.7 75.4L42 73.7l-14.8-8 33.9 6.4z" fill="#acd2b4"/><path d="M55.7 79l-7.5-.7-11.5-3.7 24.7.9 24.1 2 2.8 5.2-6-1.1-13.1-1.2-9.5-3.6z" fill="#678a6f"/><path d="M38.4 68.7L18.1 54.5l41 12.5 19.1 9.8-17.5-1.4z" fill="#cce1d1"/><path d="M61.2 68.1l3-6.8L2.6 24.4 14 40.6l16.5 12.7 14.4 9.4z" fill="#78a381"/><path d="M69.7 46.6L0 10.4l17.9 24.5 46.3 26.4 24.6 6.1z" fill="#393839"/><path d="M110 84.5l.4 1.7-11.7-4.1H88.3L86.9 80V67l1.9.4v.4l11.3 2.9-9.8 1.4 7.8 3.3-7.8.2 12.3 4.9z" fill="#cce1d1"/><path d="M111 88.4l-1-3.9 5.7-3v6.7l-3 2.6h-3.1z" fill="#3a3a3a"/><path d="M59.6 98.2l-2.1-3.1-8.1 17.3 2.2 2.8-.5.7 1.6 16-10.2-19.4 8.7-20.6 18-11.5 15 1.6v.1z" fill="#cde3d2"/><path d="M29.8 101.2l7.8-10.7 22.1-13.7 9.5 3.6L34.7 102l-7.5 8.9z" fill="#8fc19a"/><path d="M57.5 95.1l2.1 3.1v24.3l3.1 7.7-13.3-17.8z" fill="#78a381"/><path d="M25.7 95.8v3.4l6.4-8.6z" fill="#3a3a3a"/><path d="M33.4 124.6l-2.4-1.5v-8.3l1.3 3.1z" fill="#678a6f"/></g></svg>
+            <p>And of course I can develop page using Phalcon like this blog.</p>
+        </article>
+
+        <article>
+            <h2>
+                <i class="fab fa-java mr-3"></i>
+                <svg class="inline-block" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 75 90"><path d="M58.2 3.365a29.503 29.503 0 0 1-3.419 6.064A32.094 32.094 0 1 0 9.965 55.372l1.186 1.047a32.08 32.08 0 0 0 52.67-22.253c.875-8.17-1.524-18.51-5.62-30.8zM14.53 55.558a2.744 2.744 0 1 1-.404-3.857 2.744 2.744 0 0 1 .404 3.857zm43.538-9.61c-7.92 10.55-24.83 6.99-35.672 7.502 0 0-1.922.113-3.857.43 0 0 .73-.31 1.663-.663 7.614-2.65 11.213-3.16 15.838-5.54 8.708-4.427 17.322-14.122 19.112-24.2-3.313 9.695-13.373 18.032-22.53 21.418-6.276 2.313-17.614 4.566-17.614 4.566l-.457-.245c-7.714-3.75-7.952-20.457 6.077-25.845 6.143-2.366 12.02-1.067 18.654-2.65 7.084-1.683 15.28-6.99 18.615-13.916 3.73 11.08 8.224 28.422.166 39.15z" fill="#68bd45"/></svg>
+            </h2>
+            <p>Not much experienced relatively php, but I consistently learn about JAVA, especially Spring and SpringBoot.</p>
+        </article>
+
+        <article>
+            <h2><i class="fab fa-node-js text-green-600 mr-3"></i><i class="fab fa-python text-blue-700"></i></h2>
+            <p>too slightly educated to make my own web, But I briefly practice another backend.</p>
+        </article>
+
+        <article>
+            <h2><i class="fab fa-js text-yellow-500"></i></h2>
+            <p>As I web developer, I dont't think I'm enough to make some homepage as backend developer. More and more site want to render and calculate some data on front browser, not server.</p>
+            <i class="fab fa-vuejs text-green-400 mt-4"></i>
+            <p>Mainly I use Vue, as Laravel support strongly, But it's not all.</p>
+            <svg class="mt-4" id="svelte" xmlns="http://www.w3.org/2000/svg"  viewBox="-25 10 140 120"><path id="logotype" fill="#4a4a55"/>
+                <path id="back" fill="#ff3e00" d="M110.23,28.39C99.83,13.51,79.29,9.1,64.44,18.56L38.36,35.18a29.9,29.9,0,0,0-13.52,20,31.53,31.53,0,0,0,3.1,20.24,29.94,29.94,0,0,0-4.47,11.18,31.86,31.86,0,0,0,5.45,24.12c10.4,14.88,30.94,19.29,45.79,9.83L100.79,104a30,30,0,0,0,13.52-20,31.52,31.52,0,0,0-3.11-20.23,30.13,30.13,0,0,0,4.48-11.18,31.9,31.9,0,0,0-5.45-24.12"/>
+                <path id="front" fill="#fff" d="M61.89,112.16a20.73,20.73,0,0,1-22.24-8.25,19.14,19.14,0,0,1-3.27-14.5A17,17,0,0,1,37,87l.49-1.5,1.34,1A33.78,33.78,0,0,0,49,91.56l1,.29-.09,1A5.9,5.9,0,0,0,51,96.7a6.25,6.25,0,0,0,6.7,2.48,5.85,5.85,0,0,0,1.6-.7L85.34,81.86a5.42,5.42,0,0,0,2.45-3.64,5.77,5.77,0,0,0-1-4.37,6.25,6.25,0,0,0-6.7-2.48,5.72,5.72,0,0,0-1.6.7l-10,6.35a19.1,19.1,0,0,1-5.29,2.32A20.72,20.72,0,0,1,41,72.5,19.16,19.16,0,0,1,37.75,58a18,18,0,0,1,8.13-12.06L72,29.32A19.05,19.05,0,0,1,77.26,27a20.71,20.71,0,0,1,22.23,8.25,19.14,19.14,0,0,1,3.28,14.5,20.15,20.15,0,0,1-.62,2.43l-.5,1.5-1.33-1a33.78,33.78,0,0,0-10.2-5.1l-1-.29.09-1a5.86,5.86,0,0,0-1.06-3.88A6.23,6.23,0,0,0,81.49,40a5.72,5.72,0,0,0-1.6.7L53.8,57.29a5.45,5.45,0,0,0-2.45,3.63,5.84,5.84,0,0,0,1,4.38A6.25,6.25,0,0,0,59,67.78a6,6,0,0,0,1.6-.7l10-6.34a18.61,18.61,0,0,1,5.3-2.33,20.7,20.7,0,0,1,22.23,8.24,19.16,19.16,0,0,1,3.28,14.5,18,18,0,0,1-8.13,12.06L67.19,109.83a19.18,19.18,0,0,1-5.3,2.33"/>
+            </svg>
+            <p>Svelte is not much known as Vue, React, Angular, But this is relatively new frontend side language, and I use for build this blog.</p>
+            <i class="fab fa-react mt-4 text-blue-400"></i>
+            <p>Recently I deeply look inside react. I make something soon.</p>
+        </article>
+
+        <article>
+            <h2><i class="fab fa-css3-alt text-blue-600"></i></h2>
+            <p>I'm not good at design and Ui, Ux development, But I prepared for anyone ask helping some works with css.</p>
+            <svg class="mt-4" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 75 75"><defs><linearGradient x1="0" y1="-21.333" y2="85.333" id="A" x2="64" gradientUnits="userSpaceOnUse"><stop stop-color="#2383ae" offset="0%"/><stop stop-color="#6dd7b9" offset="100%"/></linearGradient></defs><path d="M16 25.6c2.133-8.533 7.467-12.8 16-12.8 12.8 0 14.4 9.6 20.8 11.2 4.267 1.067 8-.533 11.2-4.8C61.867 27.733 56.533 32 48 32c-12.8 0-14.4-9.6-20.8-11.2-4.267-1.067-8 .533-11.2 4.8zM0 44.8C2.133 36.267 7.467 32 16 32c12.8 0 14.4 9.6 20.8 11.2 4.267 1.067 8-.533 11.2-4.8-2.133 8.533-7.467 12.8-16 12.8-12.8 0-14.4-9.6-20.8-11.2-4.267-1.067-8 .533-11.2 4.8z" fill="url(#A)" fill-rule="evenodd"/></svg>
+            <p>Tailwind is I recently mainly using framework.</p>
+            <svg class="mt-4 mr-1 inline-block" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="45px" height="45px" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 373"><path fill="#00D1B2" d="M0 256L23.273 93.09L116.363 0l116.364 116.364l-69.818 69.818L256 279.272l-139.636 93.092z"/><rect x="0" y="0" width="256" height="373" fill="rgba(0, 0, 0, 0)" /></svg>
+            <svg class="mt-4 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><defs><linearGradient x1="262.624" y1="547.206" x2="375.377" y2="432.484" id="A" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#070135"/><stop offset="1" stop-color="#612d50"/></linearGradient></defs><g transform="matrix(.249635 0 0 .249239 -63.665715 -106.04659)"><rect width="128.016" height="128.546" ry="20.68" x="255.239" y="425.326" fill="url(#A)"/><path d="M320.213 480.665c3.27 0 5.748-.7 7.437-2.133s2.533-3.52 2.533-6.293c0-2.738-.844-4.818-2.533-6.24-1.7-1.458-4.168-2.187-7.437-2.187h-11.48v16.854h11.48m.7 34.827c4.167 0 7.293-.87 9.377-2.613 2.12-1.742 3.18-4.373 3.18-7.893 0-3.45-1.042-6.027-3.126-7.733-2.084-1.742-5.227-2.613-9.43-2.613h-12.18v20.854h12.18m19.293-28.64c4.455 1.28 7.904 3.645 10.347 7.093s3.664 7.68 3.665 12.694c0 7.68-2.623 13.405-7.868 17.174s-13.22 5.653-23.927 5.653h-34.436V449.84h31.15c11.173 0 19.257 1.67 24.25 5.013 5.03 3.342 7.545 8.694 7.545 16.054 0 3.876-.916 7.182-2.748 9.92-1.832 2.702-4.5 4.7-7.976 6.027" fill="#fff"/></g></svg>
+            <p>I used to use bulma, bootstrap3 or 4</p>
+        </article>
+
+        <article>
+            <h2><a href="https://github.com/sunday00" target="_blank"><i class="fab fa-github"></i></a></h2>
+            <p>Honestly, Not much for usefullthings, but I practice all the time, and I upload deligently via github.</p>
+            <p>I welcomming your opinion, and copy some code from my repository.</p>
+            <p><i class="far fa-envelope mr-1" style="font-size: 1.0rem;"></i>Contact me <a href="mailTo:kind65505256@gmail.com" class="text{theme}-accent">kind65505256@gmail.com</a></p>
+        </article>
+
+        <article>
+            <h1>avobe icons from</h1>
+            <p><a href="https://fontawesome.com" target="_blank">Fontawsome</a></p>
+            <p><a href="https://www.vectorlogo.zone/">Vectorlogo</a></p>
+        </article>
+
+
+    </div>
+</div>
+
+<script>
+    import '@fortawesome/fontawesome-free/css/all.css'
+
+    export let theme;
+    export let csrf;
+</script>
+
+<style>
+    .wrap{
+        padding: 40px;
+        text-align: right;
+    }
+
+    .title{
+        font-size: 4rem;
+        font-family: 'philosopher';
+        font-weight: bold;
+    }
+
+    .top li{
+        margin: 10px auto;
+    }
+
+    article{
+        margin-top: 30px;
+    }
+
+    h2 i{
+        font-size: 4rem;
+        font-weight: bold;
+    }
+
+    i{
+        font-size: 2.3rem;
+    }
+
+    svg{
+        width: 48px;
+        display: block;
+        margin-left: auto;
+    }
+
+    h2 svg{
+        display: inline-block;
+        width: 60px;
+    }
+
+    svg.inline-block{
+        display: inline-block;
+    }
+</style>
