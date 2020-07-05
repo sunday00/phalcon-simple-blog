5143e5d8e02e7d339a1612b0f940c16df474bbc0
sunday00 <kind65505256@gmail.com>
2020-07-05 22:08:28 +0900

    img

--- resources/components/Post/readElements/ImgBlock.svelte
+++ resources/components/Post/readElements/ImgBlock.svelte
@@ -1,5 +1,6 @@
 <div class="img-wrap my-4">
-    <img src="{data.file.url}" alt="" class:w-full="{data.stretched}" class="{data.withBackground ? `bg-${theme}-gray` : ''}"/>
+    <img src="{data.file.url}" alt="" class="{data.withBackground ? `bg-${theme}-gray` : ''}"
+         class="{data.stretched ? `w-full` : 'w-1/3 mx-auto block'}"/>
     {#if data.caption}
         <p class="text-center" theme="{theme}">[ {data.caption} ]</p>
     {/if}
